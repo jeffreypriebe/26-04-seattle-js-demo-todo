153313e8b1c23b1617be82a14bd4afaaaf3ae171
jasper <jeffreypriebe@gmail.com>
2026-04-05 17:40:00 -0700

    feat: update shadcn/ui components to base-nova (sjtodo-xw8.2)

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -1,24 +1,22 @@
-import * as React from 'react'
-import { type VariantProps } from 'class-variance-authority'
-import { cn } from '@/lib/utils'
-import { buttonVariants } from './button-variants'
+import { Button as ButtonPrimitive } from "@base-ui/react/button"
+import { type VariantProps } from "class-variance-authority"
 
-export interface ButtonProps
-  extends
-    React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {}
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "./button-variants"
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, ...props }, ref) => {
-    return (
-      <button
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    )
-  },
-)
-Button.displayName = 'Button'
+function Button({
+  className,
+  variant = "default",
+  size = "default",
+  ...props
+}: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) {
+  return (
+    <ButtonPrimitive
+      data-slot="button"
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  )
+}
 
 export { Button }
